0bc89d571419fe6b7634d3ee21fd34dc5d394841
Samudra-Ekanayake <samudra2724@gmail.com>
2024-07-09 20:25:58 +0200

    stampati i dati in index

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 <h1>Project</h1>
+
+@foreach ($catalogo as $progetti)
+
+<h3>Project Name: {{$progetti->name}}</h3>
+<p>Description: {{$progetti->description}}</p>
+<p>creation date: {{$progetti->creation_date}}</p>
+
+    
+@endforeach
 @endsection
