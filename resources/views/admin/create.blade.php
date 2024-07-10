3687358ac12aa4093aa66e0ca82667d31e6b0fd2
Samudra-Ekanayake <samudra2724@gmail.com>
2024-07-10 16:28:01 +0200

    tolto check box

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -19,18 +19,7 @@
         <label class="form-label">Is complited</label>
         <input type="text" class="form-control" name="release_year">
     </div>
-    <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-          Yes
-        </label>
-      </div>
-      <div class="form-check mb-3">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-        <label class="form-check-label" for="flexCheckChecked">
-          No
-        </label>
-      </div>
+   
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 @endsection
--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -4,8 +4,8 @@
     <form method="POST" action="{{ route('project.store') }}" class="p-5>
         @csrf
         <div class="mb-3">
-            <label class="form-label">Name</label>
-            <input type="text" class="form-control" name="name">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" name="name">
         </div>
         <div class="mb-3">
             <label class="form-label">Description</label>
@@ -19,18 +19,7 @@
             <label class="form-label">Is complited</label>
             <input type="text" class="form-control" name="release_year">
         </div>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Yes
-            </label>
-          </div>
-          <div class="form-check mb-3">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              No
-            </label>
-          </div>
+        
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 @endsection
